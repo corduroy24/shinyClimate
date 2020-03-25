4cf3ee49f33c9a7c3c42fdad6cd20cbbd1259b84
Korede Adegboye <korede.adegboye@hotmail.com>
2020-03-24 21:48:11 -0400

    Change to shiny dashboard layout

--- README.md
+++ README.md
@@ -1,1 +1,52 @@
 # Environment_Canada_Shiny_App-
+
+Travis build status CRAN status
+
+shiny app for climate chanage 
+
+
+
+Purpose of this App:
+This app was designed for two reasons:
+
+
+
+Original Data
+
+
+App skeleton
+
+
+Interface
+
+Screenshots
+Main Panel: 
+
+
+
+
+
+Gallery Panel: 
+
+
+
+
+
+
+
+
+
+
+Network Panel: 
+
+
+
+
+
+Others Panel: other stats
+
+
+
+
+
+Further projects
--- Shiny_App/ui.R
+++ Shiny_App/ui.R
@@ -8,80 +8,147 @@
 #
 
 library(shiny)
-
-# Define UI for application that draws a histogram
-shinyUI(fluidPage(
-  tags$head(
-    tags$style(HTML("
-                  .btn {
-                    height: 100px;
-                    width: 100px;
-                    border-radius: 60%;
-                    border: 1px solid red;
-                    }
-                    "))
-  ),
-    # Application title
-    titlePanel("Canadian Climate History"),
-
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            # Input: Select a dataset ----
-            selectInput("month", "Choose a month:",
-                choices = c("January", "February", "March","April","May","June","July","August","September","October","November", "December"),
-            ),
-            
-            selectInput("temp_val", "Choose a Measuremnent:",
-                choices = c("Minimum Temperature", "Maximum Temperature")
-                # choices = c("Minimum Temperature", "Mean Temperature", "Maximum Temperature")
-            ),
-            
-            selectInput("year_to_start", "Start Year", 
-                         choices = seq(1840, 1986), selected = 1980),
-            
-            verbatimTextOutput("test_1"),
-            verbatimTextOutput("test_2"),
-            verbatimTextOutput("test_3"),
-            verbatimTextOutput("test_4"),
-            
-            selectInput("prov", "Choose a province:",
-                        choices = c()
-            ),
-            selectInput("city", "Choose a city:",
-                        choices = c()
-            ),
-        ),
-        
-        mainPanel(
-            tabsetPanel(
-                tabPanel("Historical Temperature", 
-                    h1("What would you like to know?"),
-                    selectInput("qbox", "Choose one of the following",
-                                choices = c("How has climate change affected my city?",
-                                            "How has climate chnage affected my province",
-                                            "How has climate changed affected Canada"),
-                                width = "50%"
-                                ),
-                    h2("Great! What is  your current viewpoint?"),
-                    actionButton("option1", "No effect!"),
-                    actionButton("option2", "Minor effect"), 
-                    actionButton("option3", "Major effect"), 
-                    # plotOutput("reg_temp"),
-                    # plotOutput("gghist_slope_prov"),
-                    # plotOutput("boxplot_r2"),
-                    # plotOutput("hist_slope"),
-                    # plotOutput("boxplot_slope"),
-                    # plotOutput("map_ON")
-                ),
-                tabPanel("Temperature Predictions", 
-                         HTML("")),
-                tabPanel("Quality Control", 
-                         HTML("[Show data cleaning]")),
-                tabPanel("About", 
-                    HTML("Hello world")
-                )
-            )
-        )
+library(shinydashboard)
+shinyUI<- dashboardPage(
+  
+  #   tags$head(
+  #     tags$style(HTML("
+  #                   .btn {
+  #                     height: 100px;
+  #                     width: 100px;
+  #                     border-radius: 60%;
+  #                     border: 1px solid red;
+  #                     }
+  #                     "))
+  #   ),
+  dashboardHeader(title = "Climate For You"),
+  
+  #SideBar content 
+  dashboardSidebar(
+    sidebarMenu(
+      menuItem("Home", tabName = "home", icon = icon("dashboard")),
+      menuItem("Trivia", tabName = "trivia", icon = icon("th")),
+      menuItem("Preferences", tabName = "pref", icon = icon("th")),
+      menuItem("More Info", tabName = "more", icon = icon("th"))
     )
-))
+  ),
+  
+  # Body content 
+  dashboardBody(
+    # First tab content
+    tabItems(
+      tabItem(tabName = "home",
+              fluidRow(
+                box(
+                  selectInput("prov", "Choose a province:",
+                              choices = c()
+                  ),
+                  selectInput("city", "Choose a city:",
+                              choices = c()
+                  ),
+                  selectInput("temp_val", "Choose a Measuremnent:",
+                              choices = c("Average Temperature","Minimum Temperature", "Maximum Temperature"),
+                              selected = 'Average Temperature'
+                  )
+                ),
+                box(
+                  h3("Minimum - [purpose]"),
+                  background = 'red',
+                  width = 3,
+                  height = 100
+                ),
+                box(
+                  h3("Maximum - [purpose]"),
+                  background = 'red',
+                  width = 3,
+                  height = 100
+                )
+              )
+      ),
+      
+      # Second tab content
+      tabItem(tabName = "trivia",
+              fluidRow(
+                # box(plotOutput("plot1", height = 250)),
+                box(
+                  title = "Controls",
+                  sliderInput("slider", "Number of observations:", 1, 100, 50)
+                )
+              )
+      ),
+      
+      # Third tab content
+      tabItem(tabName = "pref",
+              fluidRow(
+                box(
+                  title = "Temperature",
+                  selectInput("month", "Choose a month:",
+                              choices = c("January", "February", "March","April","May","June","July","August","September","October","November", "December"),
+                              selected = 'February'
+                              ),
+                  
+                  selectInput("year_to_start", "Start Year",
+                              choices = seq(1840, 1986), 
+                              selected = 1980
+                              )
+                )
+              )
+      ), 
+      # Second tab content
+      tabItem(tabName = "more",
+              fluidRow(
+                box(
+                  sliderInput("slider", "Number of observations:", 1, 100, 50)
+                )
+              )
+      )
+      
+    )
+
+  )
+)
+# 
+# shinyUI(fluidPage(
+
+#     # Application title
+#     titlePanel("Canadian Climate History"),
+# 
+#     # Sidebar with a slider input for number of bins
+#     sidebarLayout(
+#         sidebarPanel(
+#             # Input: Select a dataset ----
+
+#         ),
+# 
+#         mainPanel(
+#             tabsetPanel(
+#                 tabPanel("Historical Temperature", 
+#                     h1("What would you like to know?"),
+#                     selectInput("qbox", "Choose one of the following",
+#                                 choices = c("How has climate change affected my city?",
+#                                             "How has climate chnage affected my province",
+#                                             "How has climate changed affected Canada"),
+#                                 width = "50%"
+#                                 ),
+#                     h2("Great! What is  your current viewpoint?"),
+#                     actionButton("option1", "No effect!"),
+#                     actionButton("option2", "Minor effect"), 
+#                     actionButton("option3", "Major effect"), 
+#                     # plotOutput("reg_temp"),
+#                     # plotOutput("gghist_slope_prov"),
+#                     # plotOutput("boxplot_r2"),
+#                     # plotOutput("hist_slope"),
+#                     # plotOutput("boxplot_slope"),
+#                     # plotOutput("map_ON")
+#                 ),
+#                 tabPanel("Temperature Predictions", 
+#                          HTML("")),
+#                 tabPanel("Quality Control", 
+#                          HTML("[Show data cleaning]")),
+#                 tabPanel("About", 
+#                     HTML("Hello world")
+#                 )
+#             )
+#         )
+#     )
+# ))
